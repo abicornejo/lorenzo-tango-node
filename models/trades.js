6987c96d4752b147ec28bb5be2d7a0ab5201161b
Lorenzo Abraham Cornejo Ortiz <abicornejo@gmail.com>
2021-01-06 21:04:59 -0600

    first commit from lorenzo

--- models/trades.js
+++ models/trades.js
@@ -3,7 +3,76 @@
 // const sequelize = new Sequelize("sqlite::memory:");
 
 // Uncomment the code below to use Mongoose ORM
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 
 // Insert your model definition below
+
+let CounterSchema = mongoose.Schema({
+    _id: { type: String, required: true },
+    seq: { type: Number, default: 0 }
+});
+
+let counter = mongoose.model('counter', CounterSchema);
+
+const schema = new mongoose.Schema(
+    {
+        id: {
+            desc: "id trade",
+            type: Number,
+            required: true,
+        },
+        type: {
+            desc: "user roles.",
+            trim: true,
+            type: String,
+            enum: ["buy", "sell"],
+            default: "buy",
+            required: true,
+        },
+        user_id: {
+            desc: "The user Id.",
+            trim: true,
+            type: Number,
+            unique: true,
+            required: true,
+        },
+        symbol: {
+            desc: "user password",
+            type: String,
+            required: true,
+        },
+        shares: {
+            desc: "The user's name.",
+            type: Number,
+            required: true,
+        },
+        price: {
+            desc: "The users's age.",
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        strict: true,
+        versionKey: true,
+        timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+    }
+);
+
+schema.pre('save', function(next) {
+    //if (this.isNew) {
+        let doc = this;
+        counter.findByIdAndUpdate({_id: 'entityId'}, {$inc: { seq: 1} }, function(error, counter)   {
+            if(error)
+                return next(error);
+            doc.id = counter.seq;
+            next();
+        });
+    /*} else {
+        next();
+    }*/
+});
+
+
+module.exports = mongoose.model("trade", schema);
--- routes/trades.js
+++ routes/trades.js
@@ -1,4 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const tradeModel = require('./../models/trades');
+
+
+router.post('/create', function(req, res, next) {
+    //res.send('<p>Hola  mundo</p>');
+
+    /**
+     * Create a trade
+     */
+    const trade = new tradeModel({
+        type: req.body.type,
+        user_id: req.body.user_id,
+        symbol: req.body.symbol,
+        shares: req.body.shares,
+        price: req.body.price
+    });
+
+    trade
+        .save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the User.",
+            });
+        });
+
+
+});
+
+router.put('/update', function(req, res, next) {
+    res.send('<p>Hola  mundo</p>');
+});
+
+router.get('/show', function(req, res, next) {
+    res.send('<p>Hola  mundo</p>');
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+    res.send('<p>Hola  mundo</p>');
+});
 
 module.exports = router;
